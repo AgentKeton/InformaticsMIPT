3e83f653de05c4e780358bcf7839b32fae0b843f
arj98 <rita.an.19@yandex.ru>
2017-12-05 16:48:45 +0300

    Create list_app.cpp

---
+++ 3sem/Templates/list_app.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "linked_list.hpp"
+
+int main() {
+    List<int> intList;
+    list_init(intList);
+    int i;
+    for(int i = 0; i < 10; ++i) {
+        list_insert(intList, i);
+    }
+    list_print(intList, std::cout);
+    std::cout << std::endl;
+    list_destroy(intList);
+
+	system("pause");
+	return 0;
+}
