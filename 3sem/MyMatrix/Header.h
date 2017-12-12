c6543035bc0e5b92a79f36fa5e1ca098a419d331
arj98 <rita.an.19@yandex.ru>
2017-12-12 16:44:26 +0300

    Update Header.h

--- 3sem/MyMatrix/Header.h
+++ 3sem/MyMatrix/Header.h
@@ -1,34 +1,38 @@
-
-
 #ifndef header_H
 #define header_H
-//Квадратная матрица
 
 #include <iostream>
 #include <string>
+//Квадратная матрица
 
 class SquareMatrix {
+
 private:
+	//умножение на число
+	friend SquareMatrix operator*(const SquareMatrix & M, const double & t);
+	friend SquareMatrix operator*(const double & t, const SquareMatrix & M);
+
+	friend std::ostream &operator<<(std::ostream &stream, const SquareMatrix & M);
 
 protected:
-	int dim;
-	int** arr2d;
 
 public:
+	int dim;
+	double** arr2d = new double*[dim];
+
 	SquareMatrix() : dim(0) {}
 	SquareMatrix(int dim_) : dim(dim_) {
-		arr2d = new int*[dim];
 		for (int i = 0; i < dim; i++) {
-			arr2d[i] = new int[dim];
+			arr2d[i] = new double[dim];
 		}
 	}
 
-	~SquareMatrix() {
-		for (int i = 0; i < dim; i++) {
-			delete arr2d[i];
-		}
-		delete arr2d;
+	/*~SquareMatrix() {
+	for (int i = 0; i < dim; i++) {
+	delete[] arr2d[i];
 	}
+	delete[] arr2d;
+	}*/
 
 	void createMatrix() {
 		for (int i = 0; i < dim; i++) {
@@ -36,7 +40,7 @@ public:
 			std::getline(std::cin, s);*/
 
 			for (int j = 0; j < dim; j++) {
-				int n;
+				double n;
 				std::cin >> n;
 				arr2d[i][j] = n;
 			}
@@ -48,18 +52,20 @@ public:
 			for (int j = 0; j < dim; j++) {
 				std::cout << arr2d[i][j] << ' ';
 			}
-			std::cout << "  "<<std::endl;
+			std::cout << "  " << std::endl;
 		}
 	}
-	
-	
+
+
 	SquareMatrix operator+(const SquareMatrix & other) {
+
 		SquareMatrix SumMatrix(dim);
+
 		for (int i = 0; i < dim; i++) {
 			for (int j = 0; j < dim; j++) {
 				SumMatrix.arr2d[i][j] = arr2d[i][j] + other.arr2d[i][j];
 			}
-		}		
+		}
 		return SumMatrix;
 	}
 
@@ -72,6 +78,55 @@ public:
 		}
 		return SumMatrix;
 	}
+
+	SquareMatrix operator=(const SquareMatrix & other) {
+		for (int i = 0; i < dim; i++) {
+			for (int j = 0; j < dim; j++) {
+				arr2d[i][j] = other.arr2d[i][j];
+			}
+		}
+		return *this;
+	}
+
+	//умножение матриц
+	SquareMatrix operator*(const SquareMatrix & other) {
+		SquareMatrix MulMatrix(dim);
+		for (int i = 0; i < dim; i++) {
+			for (int j = 0; j < dim; j++) {
+				MulMatrix.arr2d[i][j] = 0;
+				for (int r = 0; r < dim; r++) {
+					MulMatrix.arr2d[i][j] += arr2d[i][r] * other.arr2d[r][j];
+				}
+			}
+		}
+		return MulMatrix;
+	}
+
+
 };
+
+/*SquareMatrix operator*(const SquareMatrix & M, const double & t) {
+SquareMatrix MulMatrix(M.dim);
+for (int i = 0; i < M.dim; i++) {
+for (int j = 0; j < M.dim; j++) {
+MulMatrix.arr2d[i][j] = t*M.arr2d[i][j];
+}
+}
+return MulMatrix;
+}
+
+SquareMatrix operator*(const double & t, const SquareMatrix & M) {
+SquareMatrix MulMatrix(M.dim);
+for (int i = 0; i < M.dim; i++) {
+for (int j = 0; j < M.dim; j++) {
+MulMatrix.arr2d[i][j] = t*M.arr2d[i][j];
+}
+}
+return MulMatrix;
+}*/
+
+//std::ostream &operator<<(std::ostream &stream, const SquareMatrix & M) {}
+
+
 
 #endif
