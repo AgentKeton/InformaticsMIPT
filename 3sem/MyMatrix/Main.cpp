192fe9db9681943c00e086cad6e13011f7ce9f1f
arj98 <rita.an.19@yandex.ru>
2017-12-12 16:45:15 +0300

    Update Main.cpp

--- 3sem/MyMatrix/Main.cpp
+++ 3sem/MyMatrix/Main.cpp
@@ -1,13 +1,18 @@
 #include "Header.h"
 
 #include <iostream>
+#include <string>
+
 
 int main() {
-	int n = 3;
-	SquareMatrix matr(n);
-
-	matr.createMatrix();
-	matr.PrintMatrix();
+	SquareMatrix matr1(2), matr2(2), matr3(2);
+	matr1.createMatrix();
+	//matr1.createMatrix();
+	matr2.createMatrix();
+	//matr3 = matr1 - matr2;
+	double n = 2;
+	matr3 = matr2*matr1;
+	matr3.PrintMatrix();
 
 	system("pause");
 	return 0;
