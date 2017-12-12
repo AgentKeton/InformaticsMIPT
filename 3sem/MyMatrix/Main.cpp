db5c9289a1beae8391e6d0f5344bb4d9aacd14cb
arj98 <rita.an.19@yandex.ru>
2017-12-12 21:50:40 +0300

    Update Main.cpp

--- 3sem/MyMatrix/Main.cpp
+++ 3sem/MyMatrix/Main.cpp
@@ -3,16 +3,19 @@
 #include <iostream>
 #include <string>
 
-
 int main() {
 	SquareMatrix matr1(2), matr2(2), matr3(2);
+
 	matr1.createMatrix();
-	//matr1.createMatrix();
 	matr2.createMatrix();
+	(matr1 + matr2).PrintMatrix();
+	matr3.createMatrix();
 	//matr3 = matr1 - matr2;
-	double n = 2;
-	matr3 = matr2*matr1;
+	//int n = 2;
+	//matr3 = matr3*n;
+	matr3 = matr3*matr3;
 	matr3.PrintMatrix();
+	//std::cout << matr3.arr2d[1][0];
 
 	system("pause");
 	return 0;
