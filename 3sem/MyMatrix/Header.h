e8a695fc7c0a6cccf50e096d3a8b637fbee5be7e
arj98 <rita.an.19@yandex.ru>
2017-12-12 14:53:18 +0300

    Create Header.h

---
+++ 3sem/MyMatrix/Header.h
@@ -0,0 +1,54 @@
+#ifndef header_H
+#define header_H
+//Квадратная матрица
+
+#include <iostream>
+#include <string>
+
+class SquareMatrix {
+private:
+
+protected:
+	int dim;
+	int** arr2d;
+
+public:
+	SquareMatrix() : dim(0) {}
+	SquareMatrix(int dim_) : dim(dim_) {
+		arr2d = new int*[dim];
+		for (int i = 0; i < dim; i++) {
+			arr2d[i] = new int[dim];
+		}
+	}
+
+	~SquareMatrix() {
+		for (int i = 0; i < dim; i++) {
+			delete arr2d[i];
+		}
+		delete arr2d;
+	}
+
+	void createMatrix() {
+		for (int i = 0; i < dim; i++) {
+			/*std::string s;
+			std::getline(std::cin, s);*/
+
+			for (int j = 0; j < dim; j++) {
+				int n;
+				std::cin >> n;
+				arr2d[i][j] = n;
+			}
+		}
+	}
+
+	void PrintMatrix() {
+		for (int i = 0; i < dim; i++) {
+			for (int j = 0; j < dim; j++) {
+				std::cout << arr2d[i][j] << ' ';
+			}
+			std::cout << "  "<<std::endl;
+		}
+	}
+};
+
+#endif
